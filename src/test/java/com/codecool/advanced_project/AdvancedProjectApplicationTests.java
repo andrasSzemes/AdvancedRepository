4903562f280077de7fd8ba96ef934ff21393b110
AndrasSzemes <andras@szemes.eu>
2019-05-20 10:58:05 +0200

    Add basic generated structure for the app

---
+++ src/test/java/com/codecool/advanced_project/AdvancedProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.advanced_project;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvancedProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
