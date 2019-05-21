9ad3c9f47e4c9a2c1a5acffce838363fc3d94d5c
Csaba Szabó <szabcsabest@gmail.com>
2019-05-21 15:15:10 +0200

    ShoppingListDaoDb refactored

--- src/main/java/com/codecool/advanced_project/controller/ShoppingListController.java
+++ src/main/java/com/codecool/advanced_project/controller/ShoppingListController.java
@@ -1,19 +1,12 @@
 package com.codecool.advanced_project.controller;
 
-import com.codecool.advanced_project.model.*;
-import com.codecool.advanced_project.model.mapper.ShoppingListRowMapper;
+import com.codecool.advanced_project.model.ShoppingList;
 import com.codecool.advanced_project.service.dao.ShoppingListDao;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/shopping-list")
--- src/main/java/com/codecool/advanced_project/service/dao/implementation/ShoppingListDaoDb.java
+++ src/main/java/com/codecool/advanced_project/service/dao/implementation/ShoppingListDaoDb.java
@@ -24,39 +24,31 @@ public class ShoppingListDaoDb implements ShoppingListDao {
 
     @Override
     public ShoppingList getLatest(int userId) {
-        //todo get users group ids
-        String query = "SELECT group_id FROM member_groups WHERE member_id=?";
-        List<Integer> groupIds = new ArrayList<>();
-        List<Map<String, Object>> queryRows = jdbcTemplate.queryForList(query, userId);
-        for (Map<String, Object> queryRow : queryRows) {
-            groupIds.add((Integer) queryRow.get("group_id"));
+
+        List<Integer> groupIds = getGroupIds(userId);
+
+        String query = buildQuery(groupIds.size());
+        List<Integer> queryParams = getQueryParams(userId, groupIds);
+        ShoppingList shoppingList = executeQuery(query, queryParams.toArray());
+
+        if (shoppingList != null) {
+            List<LineItem> lineItems = getLineItems(shoppingList);
+            shoppingList.setLineItems(lineItems);
         }
 
-        //todo query where group id or user id matches
-        StringBuilder queryBuilder = new StringBuilder();
-        queryBuilder.append("SELECT * FROM shopping_list WHERE member_id=?");
-        for (int i = 0; i < groupIds.size(); i++) {
-            queryBuilder.append(" OR group_id=?");
-            if(i == groupIds.size() - 1) {
-                queryBuilder.append(")");
-            }
-        }
-        queryBuilder.append(" ORDER BY id DESC LIMIT 1;");
-        query = queryBuilder.toString();
-        List<Integer> queryParams = new ArrayList<>();
-        queryParams.add(userId);
-        queryParams.addAll(groupIds);
-        ShoppingList shoppingList;
-        try {
-            shoppingList = (ShoppingList) jdbcTemplate.queryForObject(query, queryParams.toArray(), new ShoppingListRowMapper());
-        } catch (EmptyResultDataAccessException e) {
-            return null;
-        }
+        return shoppingList;
+    }
 
-        //todo query to get line items by list id
+    private List<LineItem> getLineItems(ShoppingList shoppingList) {
         List<LineItem> lineItems = new ArrayList<>();
-        query = "SELECT * FROM line_item LEFT JOIN list_line_items ON(line_item.id = list_line_items.line_item_id) WHERE list_line_items.shopping_list_id=" + shoppingList.getId();
-        queryRows = jdbcTemplate.queryForList(query);
+
+        String query = "SELECT * FROM line_item " +
+                "LEFT JOIN list_line_items ON(line_item.id = list_line_items.line_item_id) " +
+                "WHERE list_line_items.shopping_list_id=" +
+                shoppingList.getId();
+
+        List<Map<String, Object>> queryRows = jdbcTemplate.queryForList(query);
+
         for (Map<String, Object> queryRow : queryRows) {
             LineItem lineItem = new LineItem();
             lineItem.setId((Integer) queryRow.get("id"));
@@ -65,8 +57,48 @@ public class ShoppingListDaoDb implements ShoppingListDao {
             lineItem.setProductId((Integer) queryRow.get("product_id"));
             lineItems.add(lineItem);
         }
-        shoppingList.setLineItems(lineItems);
+        return lineItems;
+    }
 
+    private ShoppingList executeQuery(String query, Object[] params) {
+        ShoppingList shoppingList;
+        try {
+            shoppingList = (ShoppingList) jdbcTemplate.queryForObject(query, params, new ShoppingListRowMapper());
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
         return shoppingList;
+    }
+
+    private List<Integer> getQueryParams(int userId, List<Integer> groupIds) {
+        List<Integer> queryParams = new ArrayList<>();
+        queryParams.add(userId);
+        queryParams.addAll(groupIds);
+        return queryParams;
+    }
+
+    private String buildQuery(int numOfGroupIds) {
+        String query;
+        StringBuilder queryBuilder = new StringBuilder();
+        queryBuilder.append("SELECT * FROM shopping_list WHERE member_id=?");
+        for (int i = 0; i < numOfGroupIds; i++) {
+            queryBuilder.append(" OR group_id=?");
+            if (i == numOfGroupIds - 1) {
+                queryBuilder.append(")");
+            }
+        }
+        queryBuilder.append(" ORDER BY id DESC LIMIT 1;");
+        query = queryBuilder.toString();
+        return query;
+    }
+
+    private List<Integer> getGroupIds(int userId) {
+        String query = "SELECT group_id FROM member_groups WHERE member_id=?";
+        List<Integer> groupIds = new ArrayList<>();
+        List<Map<String, Object>> queryRows = jdbcTemplate.queryForList(query, userId);
+        for (Map<String, Object> queryRow : queryRows) {
+            groupIds.add((Integer) queryRow.get("group_id"));
+        }
+        return groupIds;
     }
 }
