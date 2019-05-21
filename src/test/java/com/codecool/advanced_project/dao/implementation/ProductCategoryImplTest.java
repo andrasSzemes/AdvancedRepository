53179e81747059e653f5425e507843c276cf615a
csepdo <arodzeta@gmail.com>
2019-05-21 15:03:37 +0200

    Added empty test classes for ProductDaoImpl and ProductCategoryDaoImpl

---
+++ src/test/java/com/codecool/advanced_project/dao/implementation/ProductCategoryImplTest.java
@@ -0,0 +1,32 @@
+package com.codecool.advanced_project.dao.implementation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductCategoryImplTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void find() {
+    }
+
+    @Test
+    public void remove() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void removeAll() {
+    }
+
+    @Test
+    public void getId() {
+    }
+}
