68a8ab63e0b1cb3416fb13e6ce27d610495c7e68
Csaba Szabó <szabcsabest@gmail.com>
2019-05-22 13:58:19 +0200

    MemberGroupsDao covered with test

--- src/test/java/com/codecool/advanced_project/service/dao/implementation/LineItemDaoDbTest.java
+++ src/test/java/com/codecool/advanced_project/service/dao/implementation/LineItemDaoDbTest.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
