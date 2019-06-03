f17c563c922f2bae0a1e3dced2fb9e29dcac9e01
AndrasSzemes <andras@szemes.eu>
2019-06-03 18:26:36 +0200

    Test refactoring after changing to ORM

--- src/test/java/com/codecool/advanced_project/ShoppingListDaoIntegrityTest.java
+++ src/test/java/com/codecool/advanced_project/ShoppingListDaoIntegrityTest.java
@@ -1,82 +1,41 @@
-/*
 package com.codecool.advanced_project;
 
-import com.codecool.advanced_project.model.ShoppingList;
-import com.codecool.advanced_project.service.dao.ShoppingListDao;
-import com.codecool.advanced_project.service.dao.implementation.ShoppingListDaoDb;
-import lombok.EqualsAndHashCode;
-import net.bytebuddy.build.ToStringPlugin;
-import org.junit.jupiter.api.AfterAll;
+import com.codecool.advanced_project.entity.ShoppingListEntity;
+import com.codecool.advanced_project.repository.ShoppingListRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.sql.DataSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 class ShoppingListDaoIntegrityTest {
-
-    private JdbcConfig jdbcConfig = new JdbcConfig();
-    private DataSource dataSource = jdbcConfig.postgresqlDataSource();
-    private JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-    private ShoppingListDao shoppingListDao = new ShoppingListDaoDb(jdbcTemplate);
 
     private String shopListTable = "shopping_list";
 
+    @Autowired
+    private ShoppingListRepository shoppingListRepository;
+
     @BeforeEach
     void clear() {
-        truncateShoppingListTable();
-    }
-
-    @Test
-    void testSaveNewShoppingListAddRecordToTable() {
-        ShoppingList shoppingList = new ShoppingList(1234);
-
-        for (int i=1; i<7; i++) {
-            shoppingListDao.saveNew(shoppingList);
-            assertEquals(i, getNumberOfRowsInTable(shopListTable));
-        }
+        //sequence do not start from 1, just items are deleted
+        shoppingListRepository.deleteAll();
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1,33,456,912,Integer.MAX_VALUE})
-    void testSaveNewShoppingListWritesCorrectGroupId(int groupId) {
-        ShoppingList shoppingList = new ShoppingList(groupId);
+    @ValueSource(longs = {1,33,456,912,Long.MAX_VALUE})
+    void testSaveNewShoppingListWritesCorrectGroupId(Long groupId) {
+        ShoppingListEntity shoppingList = ShoppingListEntity.builder()
+                                                            .groupId(groupId)
+                                                            .build();
 
-        shoppingListDao.saveNew(shoppingList);
-        assertEquals(groupId, getLastInsertedListsGroupId());
+        shoppingListRepository.save(shoppingList);
+        assertNotNull(shoppingListRepository.getOne(groupId));
     }
-
-    @AfterAll
-    static void clearAfterTests() {
-        new ShoppingListDaoIntegrityTest().truncateShoppingListTable();
-    }
-
-    private int getLastInsertedListsGroupId() {
-        String sql = "SELECT group_id FROM %s " +
-                     "ORDER BY id DESC " +
-                     "LIMIT 1";
-        String formSql = String.format(sql, shopListTable);
-
-        return jdbcTemplate.queryForObject(formSql, Integer.class);
-    }
-
-    private int getNumberOfRowsInTable(String tableName) {
-        String sql = "SELECT COUNT(*) FROM %s";
-        String formSql = String.format(sql, shopListTable);
-
-        return jdbcTemplate.queryForObject(formSql, Integer.class);
-    }
-
-    private void truncateShoppingListTable() {
-        String sql = "TRUNCATE TABLE %s; ALTER SEQUENCE %s_id_seq RESTART WITH 1;";
-        String formSql = String.format(sql, shopListTable, shopListTable);
-
-        jdbcTemplate.execute(formSql);
-    }
-
-
-}*/
+}
