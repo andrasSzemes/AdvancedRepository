b2409af8da562d4991759a8288840038f4d48910
csepdo <arodzeta@gmail.com>
2019-06-03 13:57:31 +0200

    Merge conflicts solved with feature/add-register-new-product

--- src/main/java/com/codecool/advanced_project/AdvancedProjectApplication.java
+++ src/main/java/com/codecool/advanced_project/AdvancedProjectApplication.java
@@ -1,5 +1,7 @@
 package com.codecool.advanced_project;
 
+import com.codecool.advanced_project.service.ProductServiceJPA;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AdvancedProjectApplication {
 
     @Autowired
-    private ProductService productService;
+    private ProductServiceJPA productServiceJPA;
 
     public static void main(String[] args) {
         SpringApplication.run(AdvancedProjectApplication.class, args);
