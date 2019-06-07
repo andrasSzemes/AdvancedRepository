3eec783e45df3dbb7bbe3c3fadd2d9a41a6c1d8a
csepdo <arodzeta@gmail.com>
2019-06-07 09:58:53 +0200

    Added edit function to Product Classes

--- src/main/java/com/codecool/advanced_project/controller/ProductControllerJPA.java
+++ src/main/java/com/codecool/advanced_project/controller/ProductControllerJPA.java
@@ -1,11 +1,14 @@
 package com.codecool.advanced_project.controller;
 
 import com.codecool.advanced_project.entity.ProductEntity;
+import com.codecool.advanced_project.repository.ProductRepository;
 import com.codecool.advanced_project.service.ProductServiceJPA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -34,5 +37,11 @@ public class ProductControllerJPA {
     @GetMapping("")
     public List<ProductEntity> listProducts() {
         return this.productServiceJPA.getAll();
+    }
+
+    @PutMapping("")
+public void updateProduct(HttpServletResponse response, @RequestBody @Valid ProductEntity productEntity) throws IOException {
+        if (productServiceJPA.updateProduct(productEntity) == 0)
+            response.sendError(500, "Product not found");
     }
 }
--- src/main/java/com/codecool/advanced_project/repository/ProductRepository.java
+++ src/main/java/com/codecool/advanced_project/repository/ProductRepository.java
@@ -1,13 +1,17 @@
 package com.codecool.advanced_project.repository;
 
-import com.codecool.advanced_project.entity.ProductCategoryEntity;
 import com.codecool.advanced_project.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
 
     Optional<ProductEntity> findById(Long id);
 
@@ -19,5 +23,10 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     void deleteAll();
 
-
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE ProductEntity product set product.name = #{#entity.name}, product.category = #{#entity.category} where product.id = :id")
+    int updateProduct(@Param("entity") ProductEntity productEntity, @Param("id") Long id);
 }
+
+
