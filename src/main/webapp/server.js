97533aa181ecc3ba47f3853d413587275cae34f5
AndrasSzemes <andras@szemes.eu>
2019-06-07 14:27:47 +0200

    Add some styling to the index page

--- src/main/webapp/server.js
+++ src/main/webapp/server.js
@@ -2,16 +2,32 @@ let http = require('http');
 let fs = require('fs');
 
 let handleRequest = (request, response) => {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        fs.readFile('./static/templates/index.html', null, function (error, data) {
-            if (error) {
-                response.writeHead(404);
-                respone.write('Whoops! File not found!');
-            } else {
-                response.write(data);
-            }
-            response.end();
-        });
+    switch (request.url) {
+        case "/main.css" :
+            response.writeHead(200, {"Content-Type": "text/css"});
+            fs.readFile('./static/css/main.css', null, function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    respone.write('Whoops! File not found!');
+                } else {
+                    response.write(data);
+                }
+                response.end();
+            });
+            break;
+
+        default :
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            fs.readFile('./static/templates/index.html', null, function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    respone.write('Whoops! File not found!');
+                } else {
+                    response.write(data);
+                }
+                response.end();
+            });
+    }
 };
 
 http.createServer(handleRequest).listen(8081);
