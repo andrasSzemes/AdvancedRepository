fb881a2f36e4ac8d90268a0487c175de1530dd37
AndrasSzemes <andras@szemes.eu>
2019-06-07 14:45:32 +0200

    When a line is clicked, it get's archived on db. Visual: cross-out

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -14,10 +14,27 @@ function setup() {
 const addLineItem = function (lineItem) {
     const line = document.createElement('div');
     line.classList.add('line-item');
-    line.innerHTML = "<div class='quantity'>" + lineItem.quantity + "</div>" + lineItem.product.name;
+    line.innerHTML = "<div class='dot'></div><div class='quantity'>" + lineItem.quantity + "</div>" + lineItem.product.name;
+
+    if (lineItem.isArchived) {
+        line.querySelector(".dot").classList.add("slash");
+    }
 
     const shoppingList = document.querySelector('#shopping-list');
     shoppingList.appendChild(line);
+
+    line.addEventListener("click", () => {
+        if (line.querySelector(".dot").classList.contains("slash")) {
+            line.querySelector(".dot").classList.remove("slash");
+        }
+        else {
+            line.querySelector(".dot").classList.add("slash");
+        }
+        fetch(backend_URL + '/line-item/check',
+            {method: 'PUT',
+                 body: JSON.stringify(lineItem),
+                 headers: {'Content-Type': 'application/json'}})
+    })
 };
 
 window.addEventListener('load-page', function () {
