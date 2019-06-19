f8c8777f78642e496c68a3f2ca32e60f12bf4ae8
AndrasSzemes <andras@szemes.eu>
2019-06-19 15:41:28 +0200

    Refactor: structure shopping-list related css, javascript

---
+++ src/main/resources/public/js/shopping-list.js
@@ -0,0 +1,46 @@
+//Globals
+let shoppingList;
+
+function setup() {
+    const shoppingList = document.createElement("div");
+    shoppingList.setAttribute("id", "shopping-list");
+    document.querySelector("body").appendChild(shoppingList);
+}
+
+const addLineItem = function (lineItem) {
+    const line = document.createElement('div');
+    line.classList.add('line-item');
+    line.innerHTML = "<div class='dot'></div><div class='quantity'>" + lineItem.quantity + "</div>" + lineItem.product.name;
+
+    if (lineItem.isArchived) {
+        line.querySelector(".dot").classList.add("slash");
+    }
+
+    const shoppingList = document.querySelector('#shopping-list');
+    shoppingList.appendChild(line);
+
+    line.addEventListener("click", () => {
+        if (line.querySelector(".dot").classList.contains("slash")) {
+        line.querySelector(".dot").classList.remove("slash");
+    }
+else {
+        line.querySelector(".dot").classList.add("slash");
+    }
+    fetch(backend_URL + '/line-item/check',
+        {method: 'PUT',
+            body: JSON.stringify(lineItem),
+            headers: {'Content-Type': 'application/json'}})
+})
+};
+
+window.addEventListener('load-page', function () {
+    fetch(backend_URL + '/shopping-list/latest/-1', {method: 'GET'})
+        .then((response) => response.json())
+.then((responseJson) => {
+        shoppingList = responseJson;
+
+    for (let i = 0; i < shoppingList.lineItems.length; i++) {
+        addLineItem(shoppingList.lineItems[i]);
+    }
+})
+})
